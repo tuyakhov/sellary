c8e064ffe350d450590627f6a2753a4ad1f407ad
Stefan Adolf <stadolf@gmail.com>
2022-04-24 04:20:33 +0200

    rinkeby deployment
    Signed-off-by: Stefan Adolf <stadolf@gmail.com>

--- packages/contracts/scripts/deploySellary.ts
+++ packages/contracts/scripts/deploySellary.ts
@@ -27,3 +27,8 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+/*
+npx hardhat verify --network rinkeby 0x34F395227848222a254030154e1133ecddA84B0b "0xeD5B5b32110c3Ded02a07c8b8e97513FAfb883B6" "0xF4C5310E51F6079F601a5fb7120bC72a70b96e2A" "0x745861AeD1EEe363b4AaA5F1994Be40b1e05Ff90" "Sellary D_D"
+
+ */
