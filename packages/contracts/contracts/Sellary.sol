18918d86147ee7d7fee465661f72fd4da220c8ca
Stefan Adolf <stadolf@gmail.com>
2022-04-25 19:42:42 +0200

    contracts: svg decimal / datetime  display
    Signed-off-by: Stefan Adolf <stadolf@gmail.com>

--- packages/contracts/contracts/Sellary.sol
+++ packages/contracts/contracts/Sellary.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -14,6 +13,8 @@ import {IConstantFlowAgreementV1} from "@superfluid-finance/ethereum-contracts/c
 import {CFAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "hardhat/console.sol";
+import "./StringDecimals.sol";
+import "./Dates.sol";
 
 // receives salaries by DAO
 
@@ -25,7 +26,7 @@ struct SalaryPledge {
 contract Sellary is ERC721, Ownable {
     using Strings for uint256;
     using CFAv1Library for CFAv1Library.InitData;
-    
+
     ISuperfluid private _host;
     ISuperToken public _acceptedToken; // accepted token
     IConstantFlowAgreementV1 private _cfa; // the stored constant flow agreement class address
@@ -111,7 +112,6 @@ contract Sellary is ERC721, Ownable {
         _issueNFT(msg.sender, receiver, flowRate, until);
         
     }
-
             
     //use the common or predefined flow rate _acceptedToken
     function _issueNFT(address employee_, address receiver, int96 flowRate, uint256 untilTs_) internal {
@@ -195,39 +195,59 @@ contract Sellary is ERC721, Ownable {
     }
 
     function tokenURI(uint256 tokenId) public override view exists(tokenId) returns (string memory) {
-        (, uint256 dueValue, uint256 until) = metadata(tokenId);
+        (int96 nftFlowrate, uint256 dueValue, uint256 until) = metadata(tokenId);
+        
+        //todo obviously this can be anything else than dai.
+        string memory tokenSymbol = _acceptedToken.symbol();
+        string memory decToken = StringDecimals.cutAtPrecision(
+            StringDecimals.decimalString(dueValue, 18, false),
+            4
+        );
 
-        uint256 decimalVal = dueValue / 10**18;
+        string memory decFlowRate = StringDecimals.cutAtPrecision(
+            StringDecimals.decimalString(uint256(uint96(nftFlowrate)), 18, false),
+            6
+        );
+
+        (uint untilYear, uint untilMonth, uint untilDay) = Dates.timestampToDate(until);
+        string memory untilDateString = string(
+            abi.encodePacked(
+                untilYear.toString(), "/", untilMonth.toString(), "/", untilDay.toString()
+            )
+        );
 
         return string(
             abi.encodePacked(
                 bytes('data:application/json;utf8,{"name":"'),
                 abi.encodePacked(bytes("Sellary "), tokenId.toString()),
                 bytes('","description":"'),
-                abi.encodePacked(bytes('salary pledge running until '), 
-                    until.toString(), 
+                abi.encodePacked(bytes('salary pledged until '), 
+                    untilDateString, 
                     bytes('; will yield ~'), 
-                    decimalVal.toString(),
+                    decToken,
                     bytes(' '), 
-                    bytes(_acceptedToken.symbol()), 
+                    bytes(tokenSymbol), 
                     '",'),
                 bytes('"image":"data:image/svg+xml;base64,'),
-                Base64.encode(renderSVG(tokenId)),
+                Base64.encode(renderSVG(decToken, tokenSymbol, decFlowRate, untilDateString)),
                 bytes('",'),
                 // // bytes('","external_url":"'),
                 // // getExternalUrl(tokenId),
                 bytes('"attributes": ['),
                 bytes('{"display_type": "date", "trait_type": "expires",'),
+                //todo: add currency
                 abi.encodePacked('"value":',until.toString(), '}'),
                 bytes(']}')
             )
         ); 
-    }
+    }  
 
-    function renderSVG(uint256 tokenId) public view returns (bytes memory svg) {
-        (int96 nftFlowrate, uint256 dueValue, uint256 until) = metadata(tokenId);
-        uint256 _nftFlowrate = uint256(uint96(nftFlowrate));
-
+    function renderSVG( 
+         string memory decToken, 
+         string memory tokenSymbol, 
+         string memory decFlowRate, 
+         string memory untilDateString
+    ) public pure returns (bytes memory svg) { 
         return abi.encodePacked('<svg width="600" height="600" xmlns="http://www.w3.org/2000/svg">',
             '<defs><linearGradient id="gradient-fill" x1="0" y1="0" x2="800" y2="0" gradientUnits="userSpaceOnUse">',
             '<stop offset="0" stop-color="#0a515c" /> <stop offset="0.125" stop-color="#005f68"/>',
@@ -236,11 +256,11 @@ contract Sellary is ERC721, Ownable {
             '<stop offset="0.75" stop-color="#00a35d" /> <stop offset="0.875" stop-color="#00af4c" />', 
             '<stop offset="1" stop-color="#12bb33" /> </linearGradient> </defs> <rect x="0" y="0" height="600" width="600" fill="url(#gradient-fill)"/>', 
             '<text text-anchor="start" font-size="30" x="28" y="60" font-family="Arial" fill="black"> Flowrate </text>', 
-            '<text text-anchor="start" font-size="50" x="28" y="120" font-family="Arial" fill="white">', _nftFlowrate.toString(), ' DAIx/s</text>',
+            '<text text-anchor="start" font-size="50" x="28" y="120" font-family="Arial" fill="white">', decFlowRate, ' ',tokenSymbol,'/s</text>',
             '<text text-anchor="start" font-size="30" x="28" y="200" font-family="Arial" fill="black"> Yield </text>', 
-            '<text text-anchor="start" font-size="50" x="28" y="260" font-family="Arial" fill="white">', dueValue.toString(),' DAI</text>', 
-            '<text text-anchor="start" font-size="30" x="28" y="340" font-family="Arial" fill="black"> Expiry Timestamp </text>', 
-            '<text text-anchor="start" font-size="50" x="28" y="400" font-family="Arial" fill="white">', until.toString(), '</text>',
+            '<text text-anchor="start" font-size="50" x="28" y="260" font-family="Arial" fill="white">', decToken,' ',tokenSymbol,'</text>', 
+            '<text text-anchor="start" font-size="30" x="28" y="340" font-family="Arial" fill="black"> Expiry Date (Y/M/D)</text>', 
+            '<text text-anchor="start" font-size="50" x="28" y="400" font-family="Arial" fill="white">',untilDateString,'</text>',
             '</svg>'
         );
     }
---
+++ packages/contracts/contracts/StringDecimals.sol
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+library StringDecimals {
+
+    function cutAtPrecision(string memory str, uint8 precision) public pure returns (string memory) {
+        bytes memory strBytes = bytes(str);
+        bytes memory result = new bytes(strBytes.length);
+
+        uint decimalsCovered = 0;
+        uint i = 0;
+        while (i < strBytes.length && decimalsCovered < precision + 1) {
+            bytes1 b = strBytes[i];
+            if (uint8(b) == 46) {// .
+                decimalsCovered = 1;
+            } else {
+                if (decimalsCovered > 0) {
+                    decimalsCovered = decimalsCovered + 1;
+                }
+            }
+            result[i] = strBytes[i];
+            i = i + 1;
+        }
+        return string(result);
+    }
+
+    // function decimals(uint256 weiVal, uint8 decimals_) internal pure returns (uint32 abtissa, uint32 mantissa) {
+    //     uint256 _abtissa = weiVal / 10 ** 18;
+    //     abtissa = uint32(_abtissa);
+    //     uint256 _mantissa = weiVal - _abtissa * 10 ** 18;
+    //     _mantissa = _mantissa / 10 ** (18 - decimals_);
+    //     mantissa = uint32(_mantissa);
+    // }
+
+    // https://gist.github.com/wilsoncusack/d2e680e0f961e36393d1bf0b6faafba7
+    function decimalString(uint256 number, uint8 decimals, bool isPercent) public pure returns(string memory){
+        uint8 percentBufferOffset = isPercent ? 1 : 0;
+        uint256 tenPowDecimals = 10 ** decimals;
+
+        uint256 temp = number;
+        uint8 digits;
+        uint8 numSigfigs;
+        while (temp != 0) {
+            if (numSigfigs > 0) {
+                // count all digits preceding least significant figure
+                numSigfigs++;
+            } else if (temp % 10 != 0) {
+                numSigfigs++;
+            }
+            digits++;
+            temp /= 10;
+        }
+
+        DecimalStringParams memory params;
+        params.isPercent = isPercent;
+        if((digits - numSigfigs) >= decimals) {
+            // no decimals, ensure we preserve all trailing zeros
+            params.sigfigs = number / tenPowDecimals;
+            params.sigfigIndex = digits - decimals;
+            params.bufferLength = params.sigfigIndex + percentBufferOffset;
+        } else {
+            // chop all trailing zeros for numbers with decimals
+            params.sigfigs = number / (10 ** (digits - numSigfigs));
+            if(tenPowDecimals > number){
+                // number is less tahn one
+                // in this case, there may be leading zeros after the decimal place 
+                // that need to be added
+
+                // offset leading zeros by two to account for leading '0.'
+                params.zerosStartIndex = 2;
+                params.zerosEndIndex = decimals - digits + 2;
+                params.sigfigIndex = numSigfigs + params.zerosEndIndex;
+                params.bufferLength = params.sigfigIndex + percentBufferOffset;
+                params.isLessThanOne = true;
+            } else {
+                // In this case, there are digits before and
+                // after the decimal place
+                params.sigfigIndex = numSigfigs + 1;
+                params.decimalIndex = digits - decimals + 1;
+            }
+        }
+        params.bufferLength = params.sigfigIndex + percentBufferOffset;
+        return generateDecimalString(params);
+    }
+
+    // With modifications, the below taken 
+    // from https://github.com/Uniswap/uniswap-v3-periphery/blob/main/contracts/libraries/NFTDescriptor.sol#L189-L231
+
+    struct DecimalStringParams {
+        // significant figures of decimal
+        uint256 sigfigs;
+        // length of decimal string
+        uint8 bufferLength;
+        // ending index for significant figures (funtion works backwards when copying sigfigs)
+        uint8 sigfigIndex;
+        // index of decimal place (0 if no decimal)
+        uint8 decimalIndex;
+        // start index for trailing/leading 0's for very small/large numbers
+        uint8 zerosStartIndex;
+        // end index for trailing/leading 0's for very small/large numbers
+        uint8 zerosEndIndex;
+        // true if decimal number is less than one
+        bool isLessThanOne;
+        // true if string should include "%"
+        bool isPercent;
+    }
+
+    function generateDecimalString(DecimalStringParams memory params) internal pure returns (string memory) {
+        bytes memory buffer = new bytes(params.bufferLength);
+        if (params.isPercent) {
+            buffer[buffer.length - 1] = '%';
+        }
+        if (params.isLessThanOne) {
+            buffer[0] = '0';
+            buffer[1] = '.';
+        }
+
+        // add leading/trailing 0's
+        for (uint256 zerosCursor = params.zerosStartIndex; zerosCursor < params.zerosEndIndex; zerosCursor++) {
+            buffer[zerosCursor] = bytes1(uint8(48));
+        }
+        // add sigfigs
+        while (params.sigfigs > 0) {
+            if (params.decimalIndex > 0 && params.sigfigIndex == params.decimalIndex) {
+                buffer[--params.sigfigIndex] = '.';
+            }
+            buffer[--params.sigfigIndex] = bytes1(uint8(uint256(48) + (params.sigfigs % 10)));
+            params.sigfigs /= 10;
+        }
+        return string(buffer);
+    }
+}
