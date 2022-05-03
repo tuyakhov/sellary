c38448c77a92de560d0da2e92126915d5bcd2eaf
Stefan Adolf <stadolf@gmail.com>
2022-05-03 12:12:06 +0200

    encodes metadata as base64 adds RenderVars struct to stay in sol stack depth
    Signed-off-by: Stefan Adolf <stadolf@gmail.com>

--- packages/contracts/contracts/SellaryRenderer.sol
+++ packages/contracts/contracts/SellaryRenderer.sol
@@ -11,6 +11,13 @@ import "./Dates.sol";
 contract SellaryRenderer is Initializable {
   using StringsUpgradeable for uint256;
 
+  struct RenderVars {
+    string decFlowRate;
+    string tokenSymbol;
+    string decYield;
+    string untilDateString;
+  }
+
   function initialize() public initializer {}
 
   function untilDate(uint256 until) internal pure returns (string memory untilDateString) {
@@ -24,49 +31,41 @@ contract SellaryRenderer is Initializable {
 
   function metadata(
       uint256 tokenId, 
-      string memory tokenSymbol, 
+      string memory tokenSymbol_, 
       int96 nftFlowrate, 
       uint256 dueValue, 
       uint256 until
   ) public pure returns (string memory) {
 
-    //todo obviously this can be anything else than dai.
-    string memory decYield = StringDecimals.cutAtPrecision(
-        StringDecimals.decimalString(dueValue, 18, false),
-        4
-    );
-
-    string memory decFlowRate = StringDecimals.cutAtPrecision(
-        StringDecimals.decimalString(uint256(uint96(nftFlowrate)), 18, false),
-        6
-    );
-
-    string memory untilDateString = untilDate(until);
+    RenderVars memory renderVars = RenderVars({
+        decFlowRate: StringDecimals.cutAtPrecision(StringDecimals.decimalString(uint256(uint96(nftFlowrate)), 18, false), 6),
+        tokenSymbol: tokenSymbol_,
+        decYield: StringDecimals.cutAtPrecision(StringDecimals.decimalString(dueValue, 18, false),4),
+        untilDateString: untilDate(until)
+    });
 
     return string(
-        abi.encodePacked(
-            bytes('data:application/json;utf8,{"name":"'),
-            abi.encodePacked(bytes("Sellary "), tokenId.toString()),
-            bytes('","description":"'),
-            abi.encodePacked(bytes('salary pledged until '), 
-                untilDateString, 
-                bytes('; will yield ~'), 
-                decYield,
-                bytes(' '), 
-                bytes(tokenSymbol), 
-                '",'),
-            bytes('"image":"data:image/svg+xml;base64,'),
-            Base64Upgradeable.encode(renderSVG(decFlowRate, tokenSymbol,decYield,  untilDateString)),
-            bytes('",'),
-            // // bytes('","external_url":"'),
-            // // getExternalUrl(tokenId),
-            bytes('"attributes": ['),
-            bytes('{"display_type": "date", "trait_type": "expires",'),
-            //todo: add currency
-            abi.encodePacked('"value":',until.toString(), '}'),
-            bytes(']}')
-        )
-    );  
+            abi.encodePacked("data:application/json;base64,", 
+                Base64Upgradeable.encode(
+                    abi.encodePacked(
+                        bytes('{"name":"'),
+                        abi.encodePacked(bytes("Sellary "), tokenId.toString()),
+                        bytes('","description":"'),
+                        abi.encodePacked(bytes('salary pledged until '), 
+                            renderVars.untilDateString, 
+                            '; will yield ~', 
+                            renderVars.decYield,
+                            ' ', 
+                            renderVars.tokenSymbol, 
+                            '",'),
+                        bytes('"image":"data:image/svg+xml;base64,'),
+                        Base64Upgradeable.encode(renderSVG(renderVars)),
+                        bytes('","attributes": [{"display_type": "date", "trait_type": "expires","value":'),
+                        abi.encodePacked(until.toString(), '}]}')
+                    )
+                )
+            )
+    );
   }
 
 function renderBG() internal pure returns (bytes memory bg) {
@@ -81,20 +80,17 @@ function renderBG() internal pure returns (bytes memory bg) {
         );
 } 
   function renderSVG( 
-         string memory decFlowRate, 
-         string memory tokenSymbol, 
-         string memory decYield, 
-         string memory untilDateString
+         RenderVars memory renderVars
     ) public pure returns (bytes memory svg) { 
         
         return abi.encodePacked('<svg width="600" height="600" xmlns="http://www.w3.org/2000/svg">',
             renderBG(),
             '<text text-anchor="start" font-size="30" x="28" y="60" font-family="Arial" fill="black"> Flowrate </text>', 
-            '<text text-anchor="start" font-size="50" x="28" y="120" font-family="Arial" fill="white">', bytes(decFlowRate), bytes(' '),bytes(tokenSymbol),'/s</text>',
+            '<text text-anchor="start" font-size="50" x="28" y="120" font-family="Arial" fill="white">', bytes(renderVars.decFlowRate), bytes(' '),bytes(renderVars.tokenSymbol),'/s</text>',
             '<text text-anchor="start" font-size="30" x="28" y="200" font-family="Arial" fill="black"> Yield </text>', 
-            '<text text-anchor="start" font-size="50" x="28" y="260" font-family="Arial" fill="white">', decYield,bytes(' '),tokenSymbol,'</text>', 
+            '<text text-anchor="start" font-size="50" x="28" y="260" font-family="Arial" fill="white">', renderVars.decYield,bytes(' '),renderVars.tokenSymbol,'</text>', 
             '<text text-anchor="start" font-size="30" x="28" y="340" font-family="Arial" fill="black"> Expiry Date (Y/M/D)</text>', 
-            '<text text-anchor="start" font-size="50" x="28" y="400" font-family="Arial" fill="white">',bytes(untilDateString),'</text>',
+            '<text text-anchor="start" font-size="50" x="28" y="400" font-family="Arial" fill="white">',bytes(renderVars.untilDateString),'</text>',
             '</svg>'
         );
     }
